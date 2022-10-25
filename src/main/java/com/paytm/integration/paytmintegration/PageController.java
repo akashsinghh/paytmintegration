518d3177ff44886c2456ed083bc45e3ae33b9ddf
akash <ak692380@gmail.com>
2022-10-25 21:04:00 +0530

    Initial commit

---
+++ src/main/java/com/paytm/integration/paytmintegration/PageController.java
@@ -0,0 +1,18 @@
+package com.paytm.integration.paytmintegration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String homePage() {
+        System.out.println("Request for home page");
+        return "home";
+    }
+    @RequestMapping("/about")
+    public String aboutPage() {
+        System.out.println("Request for about page");
+        return "about";
+    }
+}
---
+++ src/main/java/com/paytm/integration/paytmintegration/config/App/AppConfig.java
@@ -0,0 +1,7 @@
+package com.paytm.integration.paytmintegration.config.App;
+
+public class AppConfig {
+    public static final String MID="tFETew25353128556843";
+    public static final String MKEY="EjyXnKWUCoFk%eMy";
+    public static final String WEBSITE="WEBSTAGING";
+}
